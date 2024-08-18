02d56481a3716dde1cb8fcb16ff98531d31f956b
Abdul Rafay <121513581+abdulrafay0608@users.noreply.github.com>
2024-08-18 20:33:38 +0500

    first commit

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(expressSession({
